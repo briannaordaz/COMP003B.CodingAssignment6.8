2e4889a2fa183137f3d7e7a0b22e2b14b2570638
briannaordaz <briannaordaz5@gmail.com>
2025-05-02 17:34:29 -0700

    added controllers with views

---
+++ COMP003B.CodingAssignment6.8/Migrations/20250502235035_AddPhoneToCustomer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace COMP003B.CodingAssignment6._8.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhoneToCustomer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Customers");
+        }
+    }
+}
